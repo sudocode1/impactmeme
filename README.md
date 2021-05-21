839daf8b73754dfb80eb7858f108f01e570aa36b
Roux <52894745+sudocode1@users.noreply.github.com>
2021-05-21 21:09:45 +0100

    new stuff

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # impactmeme
-make impact font memes
+make impact font memes because apparently theyre still funny
+
+## how it works
+you are asked for top text, bottom text, an image name and a font color and then the image will be created. if there are any errors they will be sent back to you.
+
+## parameters
+eg. `node index.js --larger` <br>
+<br>
+`--larger` - double the size of the canvas and the text <br>
+`--largertext` - double the size of text <br>
+`--largercanvas` - double size of canvas <br>
+`--setvals <textSize> <canvasHeight> <canvasWidth>` - set custom values for the generator 
+
+## stuff i need to do (feel free to contribute)
+- text outline
+- fix text placement with custom values (base of canvx + canvy, not just canvy)
+- allow custom urls for images because people hate local shit
+- become funny
+
---
+++ index.js
@@ -0,0 +1,34 @@
+let textSize = '20';
+let canv = {x: 200, y: 200};
+
+if (process.argv.includes('--larger')) { textSize = '40'; canv = {x: 400, y: 400} };
+if (process.argv.includes('--largercanvas')) { canv = {x: 400, y: 400} };
+if (process.argv.includes('--largertext')) { textSize = '40'; };
+
+if (process.argv[2] === ('--setvals')) {
+    textSize = parseInt(process.argv[3]);
+    canv = { x: parseInt(process.argv[4]), y: parseInt(process.argv[5]) };
+} 
+
+const { createCanvas, loadImage, Image } = require('canvas');
+const prompt = require('prompt-sync')();
+const canvas = createCanvas(canv.x, canv.y);
+const ctx = canvas.getContext('2d');
+
+let topText = prompt('top text: ');
+let bottomText = prompt('bottom text: ');
+let imgRequested = prompt('img: ');
+let textColor = prompt('text color (usually white): ')
+
+const image = require('fs').readFileSync(imgRequested);
+const img = new Image();
+img.onload = () => ctx.drawImage(img, 0, 0, canv.x, canv.y);
+img.onerror = err => { throw err };
+img.src = image;
+
+
+ctx.font = textSize + 'px Impact';
+ctx.fillStyle = textColor;
+ctx.fillText(topText, canvas.width / 2 - ctx.measureText(topText).width / 2, 25 * (canv.x / 200));
+ctx.fillText(bottomText, canvas.width / 2 - ctx.measureText(bottomText).width / 2, 195 * (canv.y / 200));
+require('fs').writeFileSync('img.png', canvas.toBuffer())
